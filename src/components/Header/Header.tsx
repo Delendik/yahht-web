a080a33fe726799a8472b1b4374e706a44a87933
Alexey Delendik <volosya007@yandex.ru>
2021-11-20 15:43:01 +0300

    add pavel

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -90,6 +90,14 @@ export default function Header() {
             src="/anna.png"
             srcSet="/anna@2x.png 2x"
           />
+          <img
+            className={styles.header__avatar}
+            width="80"
+            height="80"
+            alt="avatar"
+            src="/pavel.png"
+            srcSet="/pavel@2x.png 2x"
+          />
         </div>
 
         <Link href="/about">
